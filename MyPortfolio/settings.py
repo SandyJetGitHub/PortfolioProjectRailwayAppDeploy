4676891a4dff6a4e658d252d91b635118b581263
SandyJetGitHub <sandeshgharal27@gmail.com>
2023-05-02 10:39:14 +0530

    updated CSRF and Allowed host settings

--- MyPortfolio/settings.py
+++ MyPortfolio/settings.py
@@ -15,7 +15,8 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DJANGO_DEBUG")
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "sandeshgharal.up.railway.app"] 
+CSRF_TRUSTED_ORIGINS = ['sandeshgharal.up.railway.app']
 
 
 # Application definition

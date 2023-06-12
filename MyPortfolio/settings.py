a115d2960415b76f4dd7a9506a5ca022539528df
Sandesh Chandrakant Gharal <31091312+SandyJetGitHub@users.noreply.github.com>
2023-06-12 12:59:49 +0530

    Update settings.py
    Updated allowed hosts

--- MyPortfolio/settings.py
+++ MyPortfolio/settings.py
@@ -15,8 +15,8 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DJANGO_DEBUG")
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "sandeshgharal.up.railway.app"] 
-CSRF_TRUSTED_ORIGINS = ['sandeshgharal.up.railway.app']
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "sandeshgharal.up.railway.app", "sandeshgharalportfolio.up.railway.app"] 
+CSRF_TRUSTED_ORIGINS = ['sandeshgharal.up.railway.app', 'sandeshgharalportfolio.up.railway.app']
 
 
 # Application definition
